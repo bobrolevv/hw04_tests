bbffc1318b6baec76e9aaf328ce109b4f4773683
Artem <bobrolevv@yandex.ru>
2021-04-06 21:23:30 +0700

    20210406 test_urls done

--- posts/tests/test_urls.py
+++ posts/tests/test_urls.py
@@ -11,6 +11,7 @@ class StaticURLTests(TestCase):
     def setUpClass(cls):
         super().setUpClass()
         cls.user = User.objects.create_user(username='Vasya')
+        cls.user2 = User.objects.create_user(username='Vova')
         cls.group = Group.objects.create(
             title="Тестовая группа",
             slug="test_slug",
@@ -22,11 +23,12 @@ class StaticURLTests(TestCase):
             author=cls.user
         )
 
-
     def setUp(self):
         self.guest_client = Client()
         self.authorized_client = Client()
         self.authorized_client.force_login(self.user)
+        self.authorized_client2 = Client()
+        self.authorized_client2.force_login(self.user2)
         # self.post = Post.objects.create(
         #     text='test text',
         #     # group=self.group,
@@ -38,7 +40,7 @@ class StaticURLTests(TestCase):
                        '/group/test_slug/',
                        '/auth/signup/',
                        '/Vasya/',
-                       '/Vasya/1/edit',
+                       '/Vasya/1/',
                        ]
         for url in tested_urls:
             with self.subTest(url=url):
@@ -46,22 +48,43 @@ class StaticURLTests(TestCase):
                 self.assertEqual(response.status_code, 200, url)
 
     def test_urls_status_code_200_authorized(self):
-        """Проверка доступности страниц tested_urls авторизованному пользователю"""
+        """
+        1. Проверка доступности страниц tested_urls
+        авторизованному пользователю
+        2. Проверка доступности страницы редактирования не автору поста
+        """
+        # 1
         tested_urls = ['/',
                        '/group/test_slug/',
-                       '/new/'
+                       '/new/',
+                       '/Vasya/',
+                       '/Vasya/1/edit/',
                        ]
         for url in tested_urls:
             with self.subTest(url=url):
                 response = self.authorized_client.get(url)
-                self.assertEqual(response.status_code, 200)
+                self.assertEqual(response.status_code, 200, url)
+        # 2
+        response = self.authorized_client2.get('/Vasya/1/edit/')
+        self.assertEqual(response.status_code, 302, url)
 
     def test_task_list_url_redirect_anonymous_on_admin_login(self):
-        """Страница по адресу /new/ перенаправит анонимного пользователя
-        на страницу логина."""
+        """
+        1. Страница по адресу /new/ перенаправит анонимного пользователя
+        на страницу логина.
+        2. Проверка, правильно ли работает редирект со страницы
+        /<username>/<post_id>/edit/для тех,
+         у кого нет прав доступа к этой странице.
+        """
+        # 1
         response = self.guest_client.get('/new/', follow=True)
         self.assertRedirects(
             response, '/auth/login/?next=/new/')
+        # 2
+        response = self.authorized_client2.get('/Vasya/1/edit/',
+                                               follow=True)
+        self.assertRedirects(
+            response, '/Vasya/')
 
     def test_about_url_uses_correct_template(self):
         """Проверка шаблона для адресов из templates_url_names"""
@@ -69,6 +92,7 @@ class StaticURLTests(TestCase):
             'index.html': '/',
             'new_post.html': '/new/',
             'group.html': '/group/test_slug/',
+            'new_post.html': '/Vasya/1/edit/',
         }
         for template, reverse_name in templates_url_names.items():
             with self.subTest():
