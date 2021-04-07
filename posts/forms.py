65171d4099613d56e6ee8aac0137e2944e2adedc
Artem <bobrolevv@yandex.ru>
2021-04-07 22:48:57 +0700

    20210407-3

--- posts/forms.py
+++ posts/forms.py
@@ -7,15 +7,3 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('text', 'group')
-
-    def clean_slug(self):
-        """Обрабатывает случай, если slug не уникален."""
-        cleaned_data = super().clean()
-        slug = cleaned_data['slug']
-        if not slug:
-            title = cleaned_data['slug']
-            slug = slugify(title)[:100]
-        if Post.objects.filter(slug=slug).exists():
-            raise ValidationError(f'Адрес "{slug}" уже существует, '
-                                  'придумайте уникальное значение')
-        return slug
