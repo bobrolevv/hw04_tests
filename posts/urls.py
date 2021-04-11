9fa82dda81a1daae95020da6b7470f883d28bce0
Artem <bobrolevv@yandex.ru>
2021-04-11 23:09:46 +0700

    review done 1

--- posts/urls.py
+++ posts/urls.py
@@ -5,13 +5,12 @@ from . import views
 app_name = 'posts'
 
 urlpatterns = [
-    # path('400/', views.page_not_found, name='not'),
-    # path('500/', views.server_error, name='err'),
     path('', views.index, name='index'),
-    path('group/<slug>/', views.group_posts, name='group'),
+    path('group/<slug:slug>/', views.group_posts, name='group'),
     path('new/', views.new_post, name='new_post'),
-    path('<username>/', views.profile, name='profile'),
-    path('<username>/<int:post_id>/', views.post_view, name='post_view'),
-    path('<username>/<post_id>/edit/', views.post_edit, name='post_edit'),
-
+    path('<str:username>/', views.profile, name='profile'),
+    path('<str:username>/<int:post_id>/',
+         views.post_view, name='post_view'),
+    path('<str:username>/<int:post_id>/edit/',
+         views.post_edit, name='post_edit'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -9,11 +9,11 @@ from .models import Post, Group, User
 
 
 def index(request):
-    post_list = Post.objects.all()
+    post_list = Post.objects.all() # noqa
     paginator = Paginator(post_list, COUNT_POSTS_IN_PAGE)
     page_number = request.GET.get('page')
     page = paginator.get_page(page_number)
-    return render(request, "index.html", {"page": page})
+    return render(request, 'index.html', {'page': page})
 
 
 def group_posts(request, slug):
@@ -22,9 +22,9 @@ def group_posts(request, slug):
     paginator = Paginator(posts, COUNT_POSTS_IN_PAGE)
     page_number = request.GET.get('page')
     page = paginator.get_page(page_number)
-    return render(request, "group.html", {"group": group, "posts": posts,
-                                          "page": page})
-    # return render(request, "group.html", {"group": group, "page": page})
+    return render(request, 'group.html', {'group': group, 'posts': posts,
+                                          'page': page})
+
 
 @login_required
 def new_post(request):
@@ -34,14 +34,12 @@ def new_post(request):
         post.author = request.user
         post.save()
         return redirect('posts:index')
-    return render(request, "new_post.html", {'form': form})
+    return render(request, 'new_post.html', {'form': form})
 
 
 def profile(request, username):
     user = get_object_or_404(User, username=username)
-    post_list = Post.objects.filter(author=user)
-    # Для выборок лучше использовать related_name
-    # - не разобрался как правильно это сделать :(
+    post_list = Post.objects.filter(author=user) # noqa
     posts_count = post_list.count()
     paginator = Paginator(post_list, 10)
     page_number = request.GET.get('page')
@@ -59,7 +57,7 @@ def profile(request, username):
 
 def post_view(request, username, post_id):
     post = get_object_or_404(Post, author__username=username, id=post_id)
-    posts_count = Post.objects.filter(author=post.author).count
+    posts_count = Post.objects.filter(author=post.author).count # noqa
     context = {'post': post,
                'author': post.author,
                'posts_count': posts_count
@@ -79,17 +77,17 @@ def post_edit(request, username, post_id):
         post.save()
         return redirect('posts:post_view', username=username,
                         post_id=post_id)
-    return render(request, "new_post.html", {'form': form, 'post': post})
+    return render(request, 'new_post.html', {'form': form, 'post': post})
 
 
 def page_not_found(request, exception): # noqa
     return render(
         request,
-        "misc/404.html",
-        {"path": request.path},
+        'misc/404.html',
+        {'path': request.path},
         status=404
     )
 
 
 def server_error(request):
-    return render(request, "misc/500.html", status=500)
+    return render(request, 'misc/500.html', status=500)
